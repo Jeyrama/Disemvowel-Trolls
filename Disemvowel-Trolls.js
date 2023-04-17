cfead251fc8f7c7192e9cc1ef703446a936aab0c
Jeyrama <126204426+Jeyrama@users.noreply.github.com>
2023-04-17 08:24:07 -0700

    Update Disemvowel-Trolls.js

--- Disemvowel-Trolls.js
+++ Disemvowel-Trolls.js
@@ -1,9 +1,10 @@
 /*
 Trolls are attacking your comment section!
+A common way to deal with this situation is to remove all of 
+the vowels from the trolls' comments, neutralizing the threat.
 
-A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the threat.
-
-Your task is to write a function that takes a string and return a new string with all vowels removed.
+Your task is to write a function that takes a string 
+and return a new string with all vowels removed.
 
 For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
 
@@ -27,3 +28,17 @@ function disemvowel(str) {
     .filter(letter => !vowels.includes(letter.toLowerCase()))
     .join('');
 }
+
+// or
+
+const disemvowel = (str) => {
+  const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
+  let newStr = '';
+  for (let i = 0; i <= str.length; i++) {
+    let char = str.charAt(i);
+    if (vowels.indexOf(char) == -1) {
+      newStr += char;
+    }
+  }
+  return newStr;
+}; 
